c9f117f7b3504289a44ef0653449e955d4e3ea1f
Krzysztof <ciszak.kr@gmail.com>
2024-07-04 12:12:38 +0200

    code review adjustments

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,25 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, User } from '@prisma/client';
-import { PrismaService } from 'src/prisma.service';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersRepository {
   constructor(private prisma: PrismaService) {}
 
-  async getUsers(params: {
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.UserWhereUniqueInput;
-    where?: Prisma.UserWhereInput;
-    orderBy?: Prisma.UserOrderByWithRelationInput;
-  }): Promise<User[]> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return this.prisma.user.findMany({
-      skip,
-      take,
-      cursor,
-      where,
-      orderBy,
-    });
+  async getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 }

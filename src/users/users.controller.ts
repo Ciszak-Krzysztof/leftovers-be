3937be09c9e4c3f210a9f604426cabcc8ba90f08
Krzysztof <ciszak.kr@gmail.com>
2024-07-04 12:01:59 +0200

    add swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get users list' })
+  @ApiResponse({ status: 200, description: 'users list', type: 'User' })
   getUsers(): Promise<User[]> {
     return this.usersService.getUsers();
   }
